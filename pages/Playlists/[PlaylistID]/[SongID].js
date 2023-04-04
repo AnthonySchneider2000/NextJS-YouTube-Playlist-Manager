50f032232379eff43676a9710f1c04be042b3057
Anthony Schneider <tonyschneider3@gmail.com>
2023-04-03 23:23:11 -0500

    basics of youtube embedding

--- pages/Playlists/[PlaylistID]/[SongID].js
+++ pages/Playlists/[PlaylistID]/[SongID].js
@@ -9,14 +9,28 @@ import mongoose from "mongoose";
 export default function SongPage({ song }) {
   return (
     <div className={styles["App"]}>
-      <Sidebar
-        pageWrapId={"page-wrap"}
-        outerContainerId={"outer-container"}
-      />
+      <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
       <SearchButton />
       <div className={styles["App-header"]}>
-        <h1>{song.name}</h1>
+        <h1 className={styles["small-bottom-margin"]}>{song.name}</h1>
+        <h2 className={styles["small-margins"]}>Artist: {song.artist}</h2>
+        {/* <div className={styles["id"]}>Artist: {song.artist}</div> */}
       </div>
+      {song.youtubeLink ? (
+        <iframe
+          className={styles["youtube-embed"]}
+          width="560"
+          height="315"
+          src={song.youtubeLink}
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen="true"
+        ></iframe>
+      ) : (
+        <p className={styles["youtube-embed"]}>No video available for this song.</p>
+      )}
+      
     </div>
   );
 }
@@ -24,30 +38,43 @@ export default function SongPage({ song }) {
 export async function getServerSideProps(context) {
   await connectToDatabase();
 
-  const { PlaylistID, SongID } = context.params;
-  const playlist = await mongoose.connection.db.collection("playlists").findOne({ _id: new mongoose.Types.ObjectId(PlaylistID) });
+  const { PlaylistID, SongID } = context.params; // get the playlist id and song id from the url
 
+  const playlist = await mongoose.connection.db
+    .collection("playlists")
+    .findOne({ _id: new mongoose.Types.ObjectId(PlaylistID) });
   if (!playlist) {
     // handle playlist not found error
     return {
-      notFound: true
-    }
+      notFound: true,
+    };
   }
 
-  const song = playlist.songs.find(song => song.id === SongID);
+  // const song = playlist.songs.find(song => song._id.toString() === SongID); // find the song in the playlist
+  // if (!song) {
+  //   // handle song not found error
+  //   return {
+  //     notFound: true
+  //   }
+  // }
 
-  if (!song) {
-    // handle song not found error
-    return {
-      notFound: true
-    }
-  }
-
+  //search the songs collection for the song with the id SongID
+  const song = await mongoose.connection.db
+    .collection("songs")
+    .findOne({ _id: new mongoose.Types.ObjectId(SongID) });
+  console.log("song: ");
+  console.log(song);
   return {
     props: {
-      song: song,
+      // make the song serializable
+      song: {
+        name: song.title,
+        artist: song.artist,
+        id: song._id.toString(),
+        //if youtube link is undefined, set it to an empty string
+        youtubeLink: song.youtubeLink ?? "",
+      },
       playlistName: playlist.name,
     },
   };
 }
-
