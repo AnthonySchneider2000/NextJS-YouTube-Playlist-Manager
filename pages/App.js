5c5d8493e98eb8026d09d3cf7529960e726ffdde
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-01 20:49:47 -0500

    added back App.js

---
+++ pages/App.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import Sidebar from "./Sidebar";
+// import SearchButton from "./SearchButton";
+// //import Playlist from "./Playlist";
+// import styles from "../styles/App.module.css";
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <div className={styles["App"]}>
+//         <Sidebar
+//           pageWrapId={"page-wrap"}
+//           outerContainerId={"outer-container"}
+//         />
+//         <SearchButton />
+//         <header className={styles["App-header"]}>
+//           <h1>Music DB</h1>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+// // export default App;
