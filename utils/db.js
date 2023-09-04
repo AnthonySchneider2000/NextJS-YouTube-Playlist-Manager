b59320d6e73bc7501ca810116d09fae28672221e
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-04 15:35:54 -0500

    added temporary debugging log for db.js

--- utils/db.js
+++ utils/db.js
@@ -2,6 +2,7 @@
 import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
+  console.log('Connecting to database...');
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
