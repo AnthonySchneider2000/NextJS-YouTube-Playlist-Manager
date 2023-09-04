992cd895c1c9c723a3c0fdefbfc17ebac9e0e745
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-04 15:40:09 -0500

    (for debugging on vercel) added dummydata to login

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,11 +2,15 @@ import connectToDatabase from '@/utils/db';
 import user from "../../models/user";
 import { withSession } from "@/utils/session"
 
-connectToDatabase();
+// connectToDatabase();
 
 const handler = withSession(async (req, res) => {
   const { username, password } = req.body;
-  const newUser = await user.findOne({ username, password });
+  // const newUser = await user.findOne({ username, password });
+  const newUser = {
+    username: "tony",
+    password: "1234",
+  };
   
   if (!newUser) {
     res.status(401).json({ success: false, message: "Invalid username or password" });
