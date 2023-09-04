ae87425577f18f6cddc5b5e2d9384d54a4edfe40
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-04 17:39:24 -0500

    added noHeader prop conditioanlly renders the header depending on if the prop passed or not

--- components/Layout.js
+++ components/Layout.js
@@ -3,19 +3,26 @@ import Sidebar from "./Sidebar";
 import styles from "../styles/App.module.css";
 import SearchButton from "./SearchButton";
 
-const Layout = ({ useHeaderStyling = false, children, title }) => {
+const Layout = ({
+  useHeaderStyling = false,
+  noHeader = false,
+  title = "",
+  children,
+}) => {
   return (
     <div className={styles["App"]}>
       <Sidebar pageWrapId={"page-wrap"} outerContainerId={"outer-container"} />
       <SearchButton />
-      <header className={styles["App-header"]}>
-        <h1>{title}</h1>
-      </header>
-        {useHeaderStyling ? (
-            <main className={styles["App-header"]}>{children}</main>
-        ) : (
-            <main className={styles["App-main"]}>{children}</main> //App-main is currently not implemented
-        )}
+      {noHeader ? null : (
+        <header className={styles["App-header"]}>
+          <h1>{title}</h1>
+        </header>
+      )}
+      {useHeaderStyling ? (
+        <main className={styles["App-header"]}>{children}</main>
+      ) : (
+        <main className={styles["App-main"]}>{children}</main> //App-main is currently not implemented
+      )}
     </div>
   );
 };
