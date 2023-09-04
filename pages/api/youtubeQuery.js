34e09402592e6a70fcc7f4c0c07bbe9622bdecf2
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-04 17:11:55 -0500

    added api for searching for youtube videos

---
+++ pages/api/youtubeQuery.js
@@ -0,0 +1,24 @@
+import connectToDatabase from "@/utils/db";
+
+// searches for a song with the yotube api
+export default async function handler(req, res) {
+  const apiUrl =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" +
+    req.body.searchTerm +
+    "&key=" +
+    process.env.YOUTUBE_API_KEY;
+  fetch(apiUrl)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Failed to search for song");
+      }
+    })
+    .then((data) => {
+      res.status(200).json({ success: true, data });
+    })
+    .catch((error) => {
+      console.error("Failed to search for song:", error);
+    });
+}
