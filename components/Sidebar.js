92715fc08a518521f9c592cf70e60c499a68ef1c
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-04 18:04:45 -0500

    removed unused css

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,18 +1,17 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
-import styles from "../styles/Sidebar.module.css";
 import Link from "next/link";
 
 const Sidebar = ({isLoggedIn}) => {
   return (
     <Menu>
-      <Link href="/" className={styles["menu-item"]}>Home</Link>
-      <Link href="/Playlists" className={styles["menu-item"]}>Playlists</Link>
-      <Link href="/SubmitSong" className={styles["menu-item"]}>Submit Song</Link>
-      <Link href="/Register" className={styles["menu-item"]}>Register</Link>
-      <Link href="/Login" className={styles["menu-item"]}>Login</Link>
-      <Link href="/api/logout" className={styles["menu-item"]}>Logout</Link>
+      <Link href="/">Home</Link>
+      <Link href="/Playlists">Playlists</Link>
+      <Link href="/SubmitSong">Submit Song</Link>
+      <Link href="/Register">Register</Link>
+      <Link href="/Login">Login</Link>
+      <Link href="/api/logout">Logout</Link>
     </Menu>
   );
 };
