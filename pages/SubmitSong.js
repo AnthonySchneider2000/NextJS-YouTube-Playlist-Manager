e707db332b6b8a9f800b426bade1bb6fefcfac5b
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-04 20:36:42 -0500

    added song searching functionality

--- pages/SubmitSong.js
+++ pages/SubmitSong.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "@/styles/App.module.css";
 import Layout from "@/components/Layout";
+import playlistCSS from "@/styles/Playlists.module.css";
 
 export default function SubmitSong() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const handleSearch = async (event) => {
+    event.preventDefault(); // Prevent the form from submitting
+
+    try {
+      const response = await fetch("/api/youtubeQuery", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ searchTerm: query }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setSearchResults(data.data.items); // Assuming data.items contains search results
+      } else {
+        throw new Error("Failed to search for song");
+      }
+    } catch (error) {
+      console.error("Failed to search for song:", error);
+    }
+  };
   return (
     <Layout title="Submit Song">
       <form
@@ -10,15 +36,37 @@ export default function SubmitSong() {
         action="/api/song"
         method="post"
       >
-        {/* song name */}
         <label htmlFor="songName">Song Name:</label>
         <input type="text" id="songName" name="songName" required />
-        {/* artist name */}
         <label htmlFor="artistName">Artist Name:</label>
         <input type="text" id="artistName" name="artistName" required />
 
         <button type="submit">Submit</button>
       </form>
+      <form className={styles["registration-form"]} onSubmit={handleSearch}>
+        <label htmlFor="searchTerm">Search for a song:</label>
+        <input
+          type="text"
+          id="searchTerm"
+          name="searchTerm"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+          required
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div>
+        <h1>Search Results</h1>
+        <ul>
+          {searchResults.map((result) => (
+            <li key={result.id.videoId} className={playlistCSS["playlistListFlex"]}>
+              <a href={`/song/${result.id.videoId}`}>{result.snippet.title}</a>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+
     </Layout>
   );
 }
--- pages/api/youtubeQuery.js
+++ pages/api/youtubeQuery.js
@@ -3,7 +3,7 @@ import connectToDatabase from "@/utils/db";
 // searches for a song with the yotube api
 export default async function handler(req, res) {
   const apiUrl =
-    "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" +
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&q=" +
     req.body.searchTerm +
     "&key=" +
     process.env.YOUTUBE_API_KEY;
