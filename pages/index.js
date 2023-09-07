b9855b0d40c179551313141d5e7c5e5b6f72ccf8
Anthony Schneider <tonyschneider3@gmail.com>
2023-09-07 17:10:49 -0500

    Added help text to main page div which tells the users to use the sidebar

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,9 @@ function Home({ user }) {
   const greeting = user ? `Hello, ${user.name}!` : "YouTube Playlist Manager";
   return (
     <Layout title={greeting}>
+      <div className={styles["App-header"]}>
+        Use the sidebar on the left to<b></b>navigate to the page you want to use.
+      </div>
       {/* add the image from public/guitar.webp to the center of the screen, use theme guitar-logo */}
       <img className={styles["guitar-logo"]} src="/guitar.webp" alt="guitar logo" />
     </Layout>
